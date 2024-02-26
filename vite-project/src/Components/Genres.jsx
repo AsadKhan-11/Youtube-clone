a1e2f60a44d67f04c43dc01897c435c4b48ab462
Asad Khan <mrasad10khan@gmail.com>
2024-02-26 17:05:29 +0500

    adding content

--- vite-project/src/Components/Genres.jsx
+++ vite-project/src/Components/Genres.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import Videos from "./Videos";
 import "./Genres.css";
 function Genres() {
   return (
     <>
-      <nav className="genres">
-        <button className="genre-btn">All</button>
-        <button className="genre-btn">Music</button>
-        <button className="genre-btn">Movies</button>
-        <button className="genre-btn">Shorts</button>
-        <button className="genre-btn">Comedy</button>
-        <button className="genre-btn">Game shows</button>
-        <button className="genre-btn">Mixes</button>
-        <button className="genre-btn">Gaming</button>
+      <nav className="genres-container">
+        <nav className="genres">
+          <button className="genre-btn">All</button>
+          <button className="genre-btn">Music</button>
+          <button className="genre-btn">Movies</button>
+          <button className="genre-btn">Shorts</button>
+          <button className="genre-btn">Comedy</button>
+          <button className="genre-btn">Game shows</button>
+          <button className="genre-btn">Mixes</button>
+          <button className="genre-btn">Gaming</button>
+        </nav>
+        <div className="content">
+          <Videos />
+        </div>
       </nav>
     </>
   );
---
+++ vite-project/src/Components/Videos.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Videos.css";
+function Videos() {
+  return (
+    <>
+      <div className="container">
+        <img src="" />
+
+        <div className="info">
+          <img className="logo" />
+          <div className="txt-container">
+            <h2 className="title">Snooker moments</h2>
+            <p className="youtuber">Snooker hub</p>
+            <div className="duration">
+              <p className="views">482k views</p>
+              <p className="time">1 month ago </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Videos;
