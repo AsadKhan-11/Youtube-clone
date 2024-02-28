095f9e16d805ca8fce75a09d38e3d5fd934a1b69
Asad Khan <mrasad10khan@gmail.com>
2024-02-28 16:05:45 +0500

    setting up navbar icons

--- vite-project/src/Components/Navbar.jsx
+++ vite-project/src/Components/Navbar.jsx
@@ -6,7 +6,22 @@ function Navbar() {
       <nav className="navbar">
         <div className="icon-box">
           <div className="icons">
-            <ion-icon name="menu-outline"></ion-icon>
+            <button className="menu">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="white"
+                className="nav-icon"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               x="0px"
