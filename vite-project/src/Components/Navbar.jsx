7d79f75d7e452824ea159b4f9a6258c8b74f0676
Asad Khan <mrasad10khan@gmail.com>
2024-02-28 15:47:15 +0500

    setting up navbar icons

--- vite-project/src/Components/Navbar.jsx
+++ vite-project/src/Components/Navbar.jsx
@@ -34,7 +34,7 @@ function Navbar() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="white"
-              className="w-6 h-6"
+              className="search-icon"
             >
               <path
                 strokeLinecap="round"
@@ -43,8 +43,21 @@ function Navbar() {
               />
             </svg>
           </button>
-          <button className="mic">
-            <ion-icon name="mic-outline"></ion-icon>
+          <button className="mic ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="nav-icon"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"
+              />
+            </svg>
           </button>
         </div>
 
